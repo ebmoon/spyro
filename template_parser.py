2f310e290eb6aca928dbc8a28fc240c20155615a
Kanghee Park <kpark247@wisc.edu>
2022-03-16 13:21:05 -0500

    write ply parser

--- template_parser.py
+++ template_parser.py
@@ -1,18 +1,112 @@
 import ply.lex as lex
 import ply.yacc as yacc
 
+tokens = (
+    'SPLITTER', 'ID', 'INTEGER',
+    'LPAREN', 'RPAREN', 'ARROW', 'COMMA', 'HOLE', 'SEMI',
+    'LT', 'LE', 'GT', 'GE', 'AND', 'OR', 'NOT', 'EQ', 'NEQ',
+    'PLUS', 'MINUS', 'TIMES', 'DIV'
+)
+
+t_SPLITTER = r'\|'
+t_ID = r'[A-Za-z_][A-Za-z0-9_]*'
+t_INTEGER = r'\d+'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_ARROW = r'->'
+t_COMMA = r','
+t_HOLE = r'\?\?'
+t_SEMI = r';'
+t_LT = r'<'
+t_LE = r'<='
+t_GT = r'>'
+t_GE = r'>='
+t_AND = r'&&'
+t_OR = r'\|\|'
+t_NOT = r'!'
+t_NE = r'!='
+t_EQ = r'=='
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIV = r'/'
+
+lexer = lex.lex(debug=0)
+
+precedence = (
+    ('left', 'PLUS', 'MINUS'),
+    ('left', 'TIMES', 'DIV'),
+    ('right', 'UMINUS')
+)
+
+def p_rule_generator(p):
+    "rule : type symbol ARROW expr"
+    
+    p[0] = (p[1], p[2], p[4])
+
+def p_type(p):
+    "type : ID"
+    
+    p[0] = p[1]
+
+def p_symbol(p):
+    "symbol : ID"
+    
+    p[0] = p[1]
+
+def p_expr_uminus(p):
+    "expr : MINUS expr %prec UMINUS"
+    
+    p[0] = ('UNARY', '-', p[2])
+
+def p_expr_unaryop(p):
+    "expr : NOT expr"
+    
+    p[0] = ('UNARY', p[1], p[2])
+
+def p_expr_binop(p):
+    '''expr : expr PLUS expr
+            | expr MINUS expr
+            | expr TIMES expr
+            | expr DIV expr
+            | expr LT expr
+            | expr LE expr
+            | expr GT expr
+            | expr GE expr
+            | expr EQ expr
+            | expr NE expr
+            | expr AND expr
+            | expr OR expr'''
+
+    p[0] = ('BINOP', p[2], p[1], p[3])
+
+def p_expr_call(p):
+    "expr : ID LPAREN args RPAREN"
+
+    p[0] = ('FCALL', p[1], p[3])
+
+def p_args(p):
+    '''args : expr
+            | args COMMA expr'''
+
+    if len(p) > 2:
+        p[0] = p[1]
+        p[0].append(p[3])
+    else:
+        p[0] = [p[1]]
+
 class TemplateParser():
     def __init__(self, template):
             # Split input code into three parts
         template, variable_section = self.__split_section_from_code(template, 'var')
         template, relation_section = self.__split_section_from_code(template, 'relation')
-        # template, generator_section = self.__split_section_from_code(template, 'generator')
+        template, generator_section = self.__split_section_from_code(template, 'generator')
         # template, example_section = self.__split_section_from_code(template, 'example')
 
         self.__implenetation = template
         self.__var_decls = self.__split_var_section(variable_section)
         self.__relations = self.__split_relation_section(relation_section)
-        # self.__generators = self.__split_generator_section(generator_section)
+        self.__generators = self.__split_generator_section(generator_section)
         # self.__example_generators = self.__split_example_section(example_section)
 
     def __split_section_from_code(self, code, section_name):
@@ -35,7 +129,9 @@ class TemplateParser():
 
     def __split_generator_section(self, section_content):
         # To-Do: Implement
-        rules = [rule.split for rule in section_content.split(';')[:-1]]
+        rules = [rule.strip().split('->') for rule in section_content.split(';')[:-1]]
+
+
         return []
 
     def __split_example_section(self, section_content):
