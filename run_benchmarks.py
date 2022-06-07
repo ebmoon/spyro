cdb943401895f083a13a97d6adcb6f3d5e999342
Kanghee Park <kpark247@wisc.edu>
2022-06-07 10:55:04 -0500

    update benchmarks

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -23,7 +23,7 @@ def main():
     enable_minimization = args.enable_minimization
     minimize_terms = args.minimize_terms
 
-    files = [os.path.join(dp, f) for dp, dn, fn in os.walk("./examples") for f in fn if ('.prop' in f) and ('sum_nonlin' in f)]
+    files = [os.path.join(dp, f) for dp, dn, fn in os.walk("./examples") for f in fn if ('.prop' in f) and ('struct' in dp) and ('tree' in f)]
 
     for path in files:
         with open(path, 'r') as infile:
